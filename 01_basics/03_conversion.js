b2a26eb8b2283aad8ab7205200634724da0250db
Devanshgupta066 <devanshgupta066@gmail.com>
2025-10-03 13:57:12 +0000

    c

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,42 +1,76 @@
-//string to num
-let score = "33abc" //1st example "33" , 2nd null
+// //string to num
+// let score = "33abc" //1st example "33" , 2nd null
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let StringtoNum = Number(score);
+// let StringtoNum = Number(score);
 
-console.log(typeof StringtoNum);
-console.log(StringtoNum);
+// console.log(typeof StringtoNum);
+// console.log(StringtoNum);
 
-// "33" => 33 i.e. string
-// "33abc" => NaN - not a number
-// true => 1 , false => 0
+// // "33" => 33 i.e. string
+// // "33abc" => NaN - not a number
+// // true => 1 , false => 0
 
 
-//string or 1,0 to bool
+// //string or 1,0 to bool
 
-let isloggedIn = "dev";
+// let isloggedIn = "dev";
 
-console.log(isloggedIn);
+// console.log(isloggedIn);
 
-console.log(typeof isloggedIn);
+// console.log(typeof isloggedIn);
 
-let stringtobool = Boolean(isloggedIn);
+// let stringtobool = Boolean(isloggedIn);
 
-console.log(typeof stringtobool);
+// console.log(typeof stringtobool);
 
-console.log(stringtobool);
+// console.log(stringtobool);
 
-// 1=> true, 0 => false
-//"" => false
-//"dev" => true
+// // 1=> true, 0 => false
+// //"" => false
+// //"dev" => true
 
-//num to string
-let num1 = 7
+// //num to string
+// let num1 = 7
 
-let stringtonum = String(num1)
+// let stringtonum = String(num1)
 
-console.log(typeof stringtonum);
-console.log(stringtonum);
+// console.log(typeof stringtonum);
+// console.log(stringtonum);
 
+//*********** Operations ************//
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2);
+// console.log(3%2);
+
+let s1 = "Dev"
+let s2 = " gupta"
+
+let s3 = s1 + s2;
+console.log(s3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2" + 2);
+
+console.log(3 + 4 * 5 % 3);//avoid this , use parenthesis
+
+// console.log(+true);
+// console.log(true+);//error
+// console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+
+let count = 100;
+count++;
+console.log(count);
+//study pre increment and post increment or decrement operations
