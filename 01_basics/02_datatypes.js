934d59ab3b46c1802540e0547f9958dce4539478
Devanshgupta066 <devanshgupta066@gmail.com>
2025-10-03 13:18:04 +0000

    c

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treated as newer JS, no need since automatically strict
+
+//alert("hello");
+
+let name = "Dev gupta"
+let age = 20
+let isLoggedIn = false
+let state;
+
+
+// number => 2^53
+// bigint
+// string => " "
+// boolean => true/false
+// null => stand alone value
+// undefined => 
+// Symbol => unique
+
+console.log(typeof age);
+console.log(typeof undefined); // undefined sicne its a datatype 
+console.log(typeof null); //object
