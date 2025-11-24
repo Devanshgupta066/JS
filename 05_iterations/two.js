6e5872df05afcf76aaaffcaa91e03ca2394225c9
Devanshgupta066 <devanshgupta066@gmail.com>
2025-11-24 11:42:40 +0530

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,17 @@
+// while loop
+let i = 1
+while (i<=3) {
+    console.log(`Ring ${i}`);
+    i++;
+}
+
+console.log("");
+
+// do while loop
+// first execution then condition
+
+let score = 47
+do {
+    console.log(`Score is ${score}`);
+    score += 6;
+} while (score<=48);
